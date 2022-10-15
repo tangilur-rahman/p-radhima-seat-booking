5e8b2ade0a7036b34b4f6447bb776fd1916e3ef0
Tangilur Rahman <mohammadtangilurrahaman@gmail.com>
2022-10-16 00:03:37 +0600

    submit on heroku

---
+++ router/userRouter.js
@@ -0,0 +1,49 @@
+// external modules
+const user = require("express").Router();
+
+const userModel = require("./../model/userModel");
+
+user.get("/allDocs", async (req, res) => {
+	try {
+		const documents = await userModel.find({});
+
+		if (documents) {
+			res.status(200).json(documents);
+		} else {
+			res.status(400).json({ error: "Maintaining mode, Try again latter!" });
+		}
+	} catch (error) {
+		res.status(500).json({ error: "Maintaining mode, Try again latter!" });
+	}
+});
+
+user.post("/submit", async (req, res) => {
+	try {
+		const { getSName, getSNum, getGName, getGNum, newDate, getId, frow_where } =
+			req.body;
+
+		console.log(req.body);
+
+		await userModel.updateOne(
+			{ _id: getId },
+			{
+				$set: {
+					student_name: getSName,
+					student_number: getSNum,
+					guardian_name: getGName,
+					guardian_number: getGNum,
+					days_left: newDate,
+					frow_where
+				}
+			}
+		);
+
+		res.status(200).json({ message: "Submit successfully." });
+	} catch (error) {
+		res.status(500).json({ error: "Maintaining mode, Try again latter!" });
+	}
+});
+
+module.exports = user;
+
+// internal modules
