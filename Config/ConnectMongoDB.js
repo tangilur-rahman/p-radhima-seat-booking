5478f4237af599fea90ce3270d4f48a28781f3ef
Tangilur Rahman <mohammadtangilurrahaman@gmail.com>
2022-11-08 17:22:38 +0600

    submit on cpanel

--- Config/ConnectMongoDB.js
+++ Config/ConnectMongoDB.js
@@ -2,7 +2,8 @@ const mongoose = require("mongoose");
 
 mongoose
 	.connect(
-		"mongodb+srv://Ridhima:cA7yswlx3goDPUeZ@cluster0.r8w0ayi.mongodb.net/?retryWrites=true&w=majority"
+		process.env.mongoDB_URL ||
+			"mongodb+srv://Ridhima:cA7yswlx3goDPUeZ@cluster0.r8w0ayi.mongodb.net/?retryWrites=true&w=majority"
 	)
 	.then(() => console.log(`Connection successfully with MongoDB`))
 	.catch((error) => console.log(`connection failed ${error.message}`));
